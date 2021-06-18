f0704d6c54df4962f79723cc8f63286edbfe09a9
unknown <Admin@DESKTOP-OJEACE5.ap-southeast-1.compute.internal>
2021-06-18 19:02:17 +0530

    msg

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hi");
- res.send("This page is to test");
- res.send("Nodejs project deployment");
- res.send("using Jenkins pipeline");
- res.send("Thanks");
-
+  res.send("Hi This page is to test Nodejs project deployment using Jenkins pipeline Thanks");
 });
 
 app.listen(4001);
